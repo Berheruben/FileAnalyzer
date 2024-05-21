93e3f1741a723c9343991f364753cb9b64d308a6
Berheruben <rubenberhe56@yourdomain.com>
2024-05-21 13:34:03 +0200

    add comments

--- src/strategies/URLFileReader.ts
+++ src/strategies/URLFileReader.ts
@@ -1,6 +1,7 @@
 import { FileReaderStrategy } from './FileReaderStrategy';
 import axios from 'axios';
 
+// Implementa la strategia di lettura per i file locali.
 export class URLFileReader implements FileReaderStrategy {
     async read(path: string): Promise<string> {
         const response = await axios.get(path);
