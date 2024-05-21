d4cedd25ffbcaff689827da6321f2846bed6a26d
Berheruben <rubenberhe56@yourdomain.com>
2024-05-21 13:34:35 +0200

    add comment

--- src/strategies/LocalFileReader.ts
+++ src/strategies/LocalFileReader.ts
@@ -1,6 +1,7 @@
 import { FileReaderStrategy } from './FileReaderStrategy';
 import fs from 'fs';
 
+// Implementa la strategia di lettura per i file da URL.
 export class LocalFileReader implements FileReaderStrategy {
     async read(path: string): Promise<string> {
         return fs.promises.readFile(path, 'utf-8');
